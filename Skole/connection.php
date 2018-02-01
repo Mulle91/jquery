133a83fd88f2a7033396228b95c6bfe6dd28a3df
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2018-02-01 12:39:55 +0100

    edit code

--- Skole/connection.php
+++ Skole/connection.php
@@ -10,3 +10,39 @@ if($con->connect_error) {
 }
 echo "Connected";
 ?>
+
+<html>
+
+<head>
+
+	<title>julemandens bulevard</title>
+	<link rel="stylesheet" type="text/css" href="skoleside.css">
+</head>
+
+<body>
+
+	<h1>Tilt skolen</h1>
+
+	<h2>Kurser</h2>
+	
+	<button id="tilbage">Tilbage</button>
+	<button id="frem">Næste</button>
+
+	<script>
+		
+		function loadDoc() {
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatecahnge = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("demo").innerHtml = this.responseText;
+				}
+			};
+
+			xhttp.open("GET", "ajax_info.txt", true);
+			xhttp.send();
+		}
+
+	</script>
+
+</body>
+</html>
